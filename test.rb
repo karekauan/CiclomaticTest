c300a34505ab85a3dcfee1c74928f4efff5e6233
kauan.costa <kauan.antonye.workspace@gmail.com>
2024-12-10 13:45:46 -0300

    Trocando gem de testes

--- test.rb
+++ test.rb
@@ -1,14 +1,12 @@
-require 'minitest/autorun'
-require 'flog'
+require 'saikuro'
 
-class ComplexityTest < Minitest::Test
-  def test_cyclomatic_complexity
-    good_complexity = Flog.new.flog_method('good_complexity.rb')
-    bad_complexity = Flog.new.flog_method('bad_complexity.rb')
-
-    puts "Good complexity score: #{good_complexity}"
-    puts "Bad complexity score: #{bad_complexity}"
-
-    assert good_complexity < bad_complexity, "Good complexity should be lower than bad complexity"
-  end
+def calculate_complexity(file_name)
+  code = File.read(file_name)
+  complexity = Saikuro.calculate_cyclomatic_complexity(code)
+  puts "Complexidade do #{file_name}: #{complexity}"
+  complexity
 end
+
+puts "Teste de complexidade:"
+good_complexity = calculate_complexity('good.rb')
+bad_complexity = calculate_complexity('bad.rb')
