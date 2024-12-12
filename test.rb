bb6e3441aed13858e190f331e33f386e721aa2d4
Kauan Costa <71836200+karekauan@users.noreply.github.com>
2024-12-12 19:16:19 -0300

    Update test.rb

--- test.rb
+++ test.rb
@@ -6,19 +6,19 @@ require_relative 'good'
 
 class CalculateGradeTest < Minitest::Test
   def test_bad_calculate_grade
-    assert_equal 'A', bad_calculate_grade(95)
-    assert_equal 'B', bad_calculate_grade(85)
-    assert_equal 'C', bad_calculate_grade(75)
-    assert_equal 'D', bad_calculate_grade(65)
-    assert_equal 'F', bad_calculate_grade(55)
+    assert_equal 'A', calculate_grade(95)
+    assert_equal 'B', calculate_grade(85)
+    assert_equal 'C', calculate_grade(75)
+    assert_equal 'D', calculate_grade(65)
+    assert_equal 'F', calculate_grade(55)
   end
 
   def test_good_calculate_grade
-    assert_equal 'A', good_calculate_grade(95)
-    assert_equal 'B', good_calculate_grade(85)
-    assert_equal 'C', good_calculate_grade(75)
-    assert_equal 'D', good_calculate_grade(65)
-    assert_equal 'F', good_calculate_grade(55)
+    assert_equal 'A', calculate_grade(95)
+    assert_equal 'B', calculate_grade(85)
+    assert_equal 'C', calculate_grade(75)
+    assert_equal 'D', calculate_grade(65)
+    assert_equal 'F', calculate_grade(55)
   end
 
   def test_flog_scores
