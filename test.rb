2121d388470c207b9a9edd89de299cf389795717
Kauan Costa <71836200+karekauan@users.noreply.github.com>
2024-12-12 19:13:43 -0300

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,12 +1,29 @@
-require 'saikuro'
+# test_calculate_grade.rb
+require 'minitest/autorun'
+require 'flog'
+require_relative 'bad'
+require_relative 'good'
 
-def calculate_complexity(file_name)
-  code = File.read(file_name)
-  complexity = Saikuro.calculate_cyclomatic_complexity(code)
-  puts "Complexidade do #{file_name}: #{complexity}"
-  complexity
+class CalculateGradeTest < Minitest::Test
+  def test_bad_calculate_grade
+    assert_equal 'A', bad_calculate_grade(95)
+    assert_equal 'B', bad_calculate_grade(85)
+    assert_equal 'C', bad_calculate_grade(75)
+    assert_equal 'D', bad_calculate_grade(65)
+    assert_equal 'F', bad_calculate_grade(55)
+  end
+
+  def test_good_calculate_grade
+    assert_equal 'A', good_calculate_grade(95)
+    assert_equal 'B', good_calculate_grade(85)
+    assert_equal 'C', good_calculate_grade(75)
+    assert_equal 'D', good_calculate_grade(65)
+    assert_equal 'F', good_calculate_grade(55)
+  end
+
+  def test_flog_scores
+    bad_flog_score = Flog.calculate_score('bad.rb')
+    good_flog_score = Flog.calculate_score('good.rb')
+    assert bad_flog_score > good_flog_score
+  end
 end
-
-puts "Teste de complexidade:"
-good_complexity = calculate_complexity('good.rb')
-bad_complexity = calculate_complexity('bad.rb')
